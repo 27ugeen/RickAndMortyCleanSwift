d091eb9db47abb8d052cffc9758730eafbe46b62
Yevhen Havrysh <39010905+27ugeen@users.noreply.github.com>
2025-02-04 11:20:39 +0200

    Initial Commit

---
+++ RickAndMortyCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyCleanSwift
+//
+//  Created by GiN Eugene on 4/2/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
